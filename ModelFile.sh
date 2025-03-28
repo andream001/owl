e66e6b7463ac5c556b280f9e53ebec51fc50f443
andream001 <andrebunhak14@gmail.com>
2025-03-28 19:24:35 -0300

    feat: atualizar modelo para llama3 e criar novo arquivo de modelo

--- ModelFile.sh
+++ ModelFile.sh
@@ -1,23 +1,11 @@
-FROM gemma3:4b
-
-#Set parameters
-PARAMETER temperature 0.8
-PARAMETER stop result
-
-
-# Sets a custom system message to specify the behavior of the chat assistant
-# Leaving it blank for now.
-
-SYSTEM """ """
-
 #!/bin/zsh
 
 # variables
-model_name="gemma3:4b"
-custom_model_name="camel-gemma3"
+model_name="llama3"
+custom_model_name="camel-llama3"
 
 #get the base model
-ollama pull gemma3:4b
+ollama pull $model_name
 
 #create the model file
-ollama create $gemma3:4b -f ./gemma3modelfile
+ollama create $custom_model_name -f ./Llama3ModelFile
